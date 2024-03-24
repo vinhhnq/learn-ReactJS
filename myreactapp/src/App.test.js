394ed1098ab2768c60b005fb597a951a86aab8b9
vinhhnq <vinhng74@gmail.com>
2024-03-24 20:26:38 +0700

    24 mar 2024

--- myreactapp/src/App.test.js
+++ myreactapp/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Profile from "./App";
 
 test('renders learn react link', () => {
   render(<App />);
+  render(<Profile/>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
